8ff6e85fb14ff57d8f548d46123ec4f2f1c918af
Wally <107082066+Waldorfio@users.noreply.github.com>
2022-09-13 14:34:58 +1000

    Add remaining functions to factory

--- code.js
+++ code.js
@@ -1,19 +1,39 @@
 console.clear();
 console.log('----');
 
+function node(value,next) {   // Factory function to create new nodes
+  return {
+    value,
+    next,
+  };
+}
+
 function linkedList() {
   return {
+
     append(value) {   // adds a new node to end of list
-      // while (a condition) {
-      //   a = aList.head.next;
-      //   a.next;
-      // }
+      lastIndex = (linkedList().size() - 1);    // last Index prior to tail
+      a = aList.head;
+      indexMatch = false;
+      i = 0;
+
+      while (indexMatch == false) {
+        if (i == lastIndex) {
+          a.next = node(value,null);
+          console.log('added!');
+          indexMatch = true;
+        }
+        i += 1;
+        a = a.next;
+      }
     },
+
     prepend(value) {  // adds a new node to start of list
       newNode = node(value,null);
       newNode.next = aList.head;    // Adding the rest of aList to the newNode's next
       aList.head = newNode;         // Appending newNode to the start of the list
     },
+
     size() {          // returns the total number of nodes in the list
       // Initialise
       endOfList = false;
@@ -28,44 +48,131 @@ function linkedList() {
           i += 1;
         };
       }
-      console.log('total # of nodes: '+i)
       return i
     },
+
     head() {          // returns the first node in the list
       return aList.head.value;
     },
+
     tail() {          // returns the last node in the list
+      lastIndex = (linkedList().size() - 1);    // last Index prior to tail
+      a = aList.head;
+      indexMatch = false;
+      i = 0;
+
+      while (indexMatch == false) {
+        if (i == lastIndex) {
+          lastVal = a.value;
+          console.log('last val: '+lastVal);
+          indexMatch = true;
+          return lastVal
+        }
+        i += 1;
+        a = a.next;
+      }
     },
+
     at(index) {       // returns the node at a given index
+      a = aList.head;
+      indexMatch = false;
+      i = 0;
+
+      while (indexMatch == false) {
+        if (i == index) {
+          console.log('node: '+a.value);
+          indexMatch = true;
+          return a
+        }
+        i += 1;
+        a = a.next;
+      }
     },
+
     pop() {           // removes last element from list
+      lastIndex = (linkedList().size() - 1);    // last Index prior to tail
+      a = aList.head;
+      indexMatch = false;
+      i = 0;
+
+      while (indexMatch == false) {
+        if (i == lastIndex-1) {
+          a.next = null;
+          console.log('popped!');
+          indexMatch = true;
+        }
+        i += 1;
+        a = a.next;
+      }
     },
+
     contains(value) { // returns true if passed in value is in the list
+      a = aList.head;
+      indexMatch = false;
+      i = 0;
+
+      while (indexMatch == false) {
+        if (a == null) {
+          indexMatch = true;
+          console.log(false);
+          return false
+        }
+        if (value == a.value) {
+          indexMatch = true;
+          console.log(true);
+          return true
+        }
+        a = a.next;
+        i += 1;
+      }
     },
+
     find(value) {     // returns index of value
+      a = aList.head;
+      indexMatch = false;
+      i = 0;
+
+      while (indexMatch == false) {
+        if (a == null) {
+          indexMatch = true;
+          console.log(value+' not found!');
+          return false
+        }
+        if (value == a.value) {
+          indexMatch = true;
+          console.log(value+' found at idx '+i);
+          return i
+        }
+        a = a.next;
+        i += 1;
+      }
     },
+
     toString() {      // converts linkedList to string
+      a = aList.head;
+      indexMatch = false;
+      finalString = '';
+
+      while (indexMatch == false) {
+        if (a == null) {
+          indexMatch = true;
+          finalString = finalString + 'null';
+          console.log(finalString);
+          return finalString
+        }
+        else {
+          indexMatch = false;
+          finalString = finalString + '( '+a.value+' ) -> ';
+        }
+        a = a.next;
+      }
     }
-  };
+
+  }; // ( value ) -> ( value ) -> ( value ) -> null
 }
 
-function node(value,next) {   // Factory function to create new nodes
-  return {
-    value,
-    next,
-  };
-}
-
-const aList = {   // Initialis empty list
-  head: {}        // Add empty head object
-}
-aList.head = node(null,null); // Add tail object to the end
-
-
-
-
-//        Example output object for a linked ist:
-const aList2 = {
+//        Example list: ( 6 ) -> ( 10 ) -> ( 12 ) -> ( 13 ) -> null
+const aList = {
   head: {
       value: 6,
       next: {
@@ -80,3 +187,15 @@ const aList2 = {
       }
   }
 }
+
+// Testing
+linkedList().append(7);       // Output: added!
+linkedList().prepend(9);      // Output: 
+linkedList().size();          // Output: return: total size
+linkedList().head();          // Output: 
+linkedList().tail();          // Output: 
+linkedList().at(3);           // Output: node: 12
+linkedList().pop();           // Output: popped!
+linkedList().contains(3);     // Output: true
+linkedList().find(5);         // Output: 5 not found!
+linkedList().toString();      // Output: ( 9 ) -> ( 6 ) -> ( 10 ) -> ( 12 ) -> ( 3 ) -> null
